bbb2b92e567b08725bf159ae21eea74b074d9086
shristichaubey02 <shristishekhar02@gmail.com>
2026-08-17 01:24:05 +0530

    Fix cart navigation wait

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,14 +1,12 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.base_page import BasePage
 
 
 class InventoryPage(BasePage):
 
-    CART_BUTTON = (
-        By.CLASS_NAME,
-        "shopping_cart_link"
-    )
+    CART_BUTTON = (By.CLASS_NAME, "shopping_cart_link")
 
     def add_product(self, product_name):
         product_id = product_name.lower().replace(" ", "-")
@@ -22,3 +20,10 @@ class InventoryPage(BasePage):
 
     def open_cart(self):
         self.click(self.CART_BUTTON)
+
+        WebDriverWait(
+            self.driver,
+            self.timeout
+        ).until(
+            lambda driver: "cart" in driver.current_url
+        )
